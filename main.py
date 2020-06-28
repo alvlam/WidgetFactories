a2d1271f6521f26a0c76561f96093c013b07f247
Alvin <alvlam@gmail.com>
2020-06-28 07:15:38 +0000

    truck moves, collides with factory that says hello (buggy)

--- main.py
+++ main.py
@@ -33,15 +33,12 @@ IMG_FACTORY = load_image_file("img/factory64px.png", (128,128))
 IMG_TRUCK = load_image_file("img/truck64px.png")
 # Icons made by <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 
-def factory(loc):
-    gameDisplay.blit(IMG_FACTORY, (loc, GROUND - IMG_FACTORY.get_height()))
-
-
 #------ Sprite definitions ------------------------------
 class Truck(pygame.sprite.Sprite):
-    def __init__(self, locX, speed):
+    def __init__(self, locX=0, speed=1, orientation="RIGHT"):
         pygame.sprite.Sprite.__init__(self)
         self.image = IMG_TRUCK
+        self.orientation = orientation
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
 
@@ -50,17 +47,35 @@ class Truck(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(location)
         self.set_target(self.pos)
         self.speed = speed
-        
-        self.autoMoving = False
-
+    
+    def set_direction(self, direction):
+        if direction.upper() == "LEFT":
+            if self.orientation == "RIGHT":
+              self.image = pygame.transform.flip(self.image, True, False)
+            self.orientation = "LEFT"
+        else:
+            if self.orientation == "LEFT":
+              self.image = pygame.transform.flip(self.image, True, False)
+            self.orientation = "RIGHT"
+    
     def set_target(self, pos):
         if pos[0] > 0 and pos[0] < DISPLAY_WIDTH: #and pos[1] > 0 and pos[1] < DISPLAY_HEIGHT:
+            # set orientation and flip image if necessary
+            if pos[0] < self.pos[0]:
+              self.set_direction("LEFT")
+            else:
+              self.set_direction("RIGHT")
             self.target = pygame.math.Vector2((pos[0], self.pos[1]))
         else:
             self.target = self.pos
     
     def move(self, vector):
-        self.set_target((self.rect.left+(vector[0]*self.speed),self.rect.top+(vector[1]*self.speed)))
+      # set orientation and flip image if necessary
+      if vector[0] < 0:
+        self.set_direction("LEFT")
+      else:
+        self.set_direction("RIGHT")
+      self.set_target((self.rect.left+(vector[0]*self.speed),self.rect.top+(vector[1]*self.speed)))
     
     def update(self):
         vector = self.target - self.pos
@@ -77,14 +92,29 @@ class Truck(pygame.sprite.Sprite):
             self.autoMoving = False
 
         self.rect.topleft = list(int(v) for v in self.pos)
-    
-    def set_direction(self, direction):
-        if direction == "left":
-          if self.orientation == "Right":
-        self.screen.blit(self.image, self.rect)
-    elif self.orientation == "Left":
-        self.screen.blit(pygame.transform.flip(self.image, False, True), self.rect)
 
+class Factory(pygame.sprite.Sprite):
+    def __init__(self, locX=0):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = IMG_FACTORY
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
+        location = (locX, GROUND - self.image.get_height())
+        self.rect.topleft = location
+    
+    # based on https://stackoverflow.com/questions/60997970/how-to-add-a-text-speech-in-pygame
+    def sayHello(self, font : str, size : int, text : str, color, bold : bool):
+        font = pygame.font.SysFont(font, size)
+        font.set_bold(bold)
+        textSurf = font.render(text, True, color, WHITE).convert_alpha()
+        textSize = textSurf.get_size()   
+        bubbleSurf = pygame.Surface((textSize[0]*2, textSize[1]*2))
+        bubbleRect = bubbleSurf.get_rect()
+        pygame.draw.rect(bubbleSurf, color, bubbleRect, 1)
+        bubbleSurf.blit(textSurf, textSurf.get_rect(center = bubbleRect.center))
+        bubbleRect.center = (self.rect.center[0], self.rect.center[1]-100)
+        gameDisplay.blit(bubbleSurf, bubbleRect)
 
 # Initialize pygame
 pygame.init()
@@ -94,8 +124,22 @@ pygame.display.set_caption('Silly Inventions!')
 
 clock = pygame.time.Clock()
 
-# Create sprites
-truck = pygame.sprite.GroupSingle(Truck(50,1))
+# Create the player
+truck = Truck(50)
+factory1 = Factory(200)
+
+# Create groups to hold enemy sprites and all sprites
+# - factories is used for collision detection and position updates
+# - allSprites is used for rendering
+factories = pygame.sprite.Group()
+factories.add(factory1)
+
+all_sprites = pygame.sprite.Group()
+for f in factories:
+    all_sprites.add(f)
+
+# truck on top of factories
+all_sprites.add(truck)
 
 done = False
 
@@ -109,23 +153,29 @@ while not done:
     # check pressed keys and move the player around
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        truck.sprite.move((-1,0))
+        truck.move((-1,0))
     elif keys[pygame.K_RIGHT]:
-        truck.sprite.move((1,0))
+        truck.move((1,0))
     
     # checking pressed mouse and move the player around
     click = pygame.mouse.get_pressed()
     if click[0] == True: # evaluate left button
-        truck.sprite.set_target(pygame.mouse.get_pos())
+        truck.set_target(pygame.mouse.get_pos())
     
     # Updates
-    truck.sprite.update()
+    truck.update()
 
     # init background
     gameDisplay.fill(WHITE)
     pygame.draw.line(gameDisplay, BLACK, (0, GROUND), (DISPLAY_WIDTH, GROUND), 5)    
-    factory(300)
-    truck.draw(gameDisplay)
+
+    all_sprites.draw(gameDisplay)
+
+    # Check if truck has collided with a factory
+    collided_factories = pygame.sprite.spritecollide(truck,factories,False)
+    for factory in collided_factories:
+        # If so, then factory should say hello
+        factory.sayHello('Arial', 18, 'Hello!', BLACK, False)
 
      # Update the display
     pygame.display.update()
